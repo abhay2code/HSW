8ade14e66dbd490489a83a11e14c64119cca6489
Abhay Singh <coding4decoding@gmail.com>
2023-04-22 09:57:25 +0530

    first contribution

---
+++ Subtraction.py
@@ -0,0 +1,5 @@
+# This is a Feature of Substraction i.e. Difference of two number. 
+
+def subtract(x, y):       # Here we stored our numbers in variables (x and y).
+    return x - y                # Here main Substraction is performed. 
+
