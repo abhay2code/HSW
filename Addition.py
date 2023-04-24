2325d8ddaf4fbccc9b52ce9c34edfe7cd668ead7
Abhay Singh <coding4decoding@gmail.com>
2023-04-24 18:56:50 +0530

    Hello

--- Addition.py
+++ Addition.py
@@ -3,3 +3,6 @@
 def add(x, y):   # Here we stored our numbers in variables (x and y).
     
     return x + y            # Here Process Addition is performed. 
+
+
+         # This function of Addition is contributed by Abhay Singh
--- Subtraction.py
+++ Subtraction.py
@@ -1,5 +1,6 @@
-# This is a Feature of Substraction i.e. Difference of two number. 
+# This is a Feature of Subtraction i.e. Difference of two number. 
 
 def subtract(x, y):       # Here we stored our numbers in variables (x and y).
-    return x - y                # Here main Substraction is performed. 
+    return x - y                # Here main Subtraction is performed. 
 
+         # This function of Subtraction is contributed by Abhay Singh
